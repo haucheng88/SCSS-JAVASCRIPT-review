77b80ff6d3756ead81d69edf056674f535f9bcfa
Hau Cheng <33608167+haucheng88@users.noreply.github.com>
2021-04-16 06:53:57 -0400

    Adding hamburger menus and basic css
    Enhancing the webpage by adding css and javascript files.

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector(".overlay");
+
+btnHamburger.addEventListener("click", function(){
+    console.log("open hamburger")
+
+    if(header.classList.contains('open')){ //Close Hamburger Menu
+        header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+    } else {//Open Hamburger Menu
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }
+});
