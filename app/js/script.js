42f5dffc5a36d50bd64b77ee17f5ad900ffd637a
Hau Cheng <33608167+haucheng88@users.noreply.github.com>
2021-05-02 14:55:37 -0400

    Finishing up the Hamburger Menu
    Adding more features and refactor the code. Updating to keep track of progress.

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,26 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener("click", function(){
     console.log("open hamburger")
 
-    if(header.classList.contains('open')){ //Close Hamburger Menu
+    if (header.classList.contains('open')){ // Close Hamburger Menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
-    } else {//Open Hamburger Menu
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        })
+    } 
+    else { // Open Hamburger Menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
     }
 });
